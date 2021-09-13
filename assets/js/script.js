88617e62f10b76f99a30156ad8cc52e6ff6b5b9a
azuryte5 <85147307+azuryte5@users.noreply.github.com>
2021-09-13 19:31:22 -0400

    Added another button setting up id's

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,11 +16,14 @@
 //     };
 // };
 // var startCountdown = setInterval(countdown, 1000);
+
 // 1)Id the tag, get doc to look for it. Give it a variable
 // 2)Define what you want it to do with an id/var
 //3) create a function to repeat an adding of code when run
 //4) run a new var with style/text. use appendchild to stick it somewhere
 //5) pick the button that will kick off and how/function
+
+
 var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
